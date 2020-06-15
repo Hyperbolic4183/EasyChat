d2b0af3a720f57c791d3715682951fefb2b536be
otsuka.amane <otsuka.amane@gmail.com>
2020-06-15 13:24:19 +0900

    Initial Commit

---
+++ EasyChatApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EasyChatApp
+//
+//  Created by 大塚周 on 2020/06/15.
+//  Copyright © 2020 大塚周. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
